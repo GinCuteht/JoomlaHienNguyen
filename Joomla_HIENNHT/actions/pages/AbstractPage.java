41741f647d16240fcebd779ccdb2fc9f6b3cc84d
Le Long Bao <Le Long Bao@192.168.0.108>
2017-11-20 23:25:05 +0700

    HienNguyen updated AbstractPage.

--- Joomla_HIENNHT/actions/pages/AbstractPage.java
+++ Joomla_HIENNHT/actions/pages/AbstractPage.java
@@ -1,5 +1,121 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 public class AbstractPage {
 
+	WebDriver driver;
+
+	public void openURL(WebDriver driver, String url) {
+		driver.get(url);
+	}
+
+	public String getURL(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+
+	public String getTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+
+	public void backURL(WebDriver driver) {
+		driver.navigate().back();
+	}
+
+	public void forwardURL(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+
+	public void refreshURL(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+
+	public void clickToElement(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		element.click();
+	}
+
+	public void clickToElement(WebDriver driver, String locator, String value) {
+		locator = String.format(locator, value);
+		WebElement element = driver.findElement(By.xpath(locator));
+		element.click();
+	}
+
+	public void sendkeyToElement(WebDriver driver, String locator, String value) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		element.clear();
+		element.sendKeys(value);
+	}
+
+	public String getTextOfElement(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.getText();
+	}
+
+	public String getTextOfElement(WebDriver driver, String locator, String value) {
+		locator = String.format(locator, value);
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.getText();
+	}
+
+	public String getAttributeOfElement(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.getAttribute(locator);
+	}
+
+	public void uncheckTheCheckbox(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		if (element.isSelected()) {
+			element.click();
+		}
+	}
+
+	public boolean isControlDisplayed(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.isDisplayed();
+	}
+
+	public boolean isControlDisplay(WebDriver driver, String locator, String value) {
+		locator = String.format(locator, value);
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.isDisplayed();
+	}
+
+	public boolean isControlSelected(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.isSelected();
+	}
+
+	public boolean isControlSelected(WebDriver driver, String locator, String value) {
+		locator = String.format(locator, value);
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.isSelected();
+	}
+
+	public boolean isEnableElement(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.isEnabled();
+	}
+
+	public boolean isEnableElement(WebDriver driver, String locator, String value) {
+		locator = String.format(locator, value);
+		WebElement element = driver.findElement(By.xpath(locator));
+		return element.isEnabled();
+	}
+
+	public void selectItemInDropdown(WebDriver driver, String locator, String value) {
+		Select element = new Select(driver.findElement(By.xpath(locator)));
+		element.deselectByVisibleText(value);
+	}
+	
+	public String verifyDropdownSelected(WebDriver driver, String locator) {
+		WebElement element = driver.findElement(By.xpath(locator));
+		Select dropdownSelected = new Select(element);
+		return dropdownSelected.getFirstSelectedOption().getText();
+	}
+	
+	
 }
