1d1d9b7d75a37b8a15614ff600a041f769c92b98
sherry <sherry@CPU004Sherry>
2017-11-24 17:29:30 +0700

    HienNguyen added 2 interfaces: HomePageUI and LoginPageUI.

---
+++ Joomla_HIENNHT/interfaces/Joomla/HomePageUI.java
@@ -0,0 +1,7 @@
+package Joomla;
+
+public class HomePageUI {
+	public static String MENU_CONTENT = "//a[@class='dropdown-toggle' and contains(text(),'%s')]";
+	public static String DROPDOWN_MENU_ARTICLES = "//a[@class='dropdown-toggle menu-article' and contains(text(),'Articles')]";
+	public static String MENU_ADD_NEW_ARTICLE = "//*[@id='nav-empty']/li/a";
+}
--- Joomla_HIENNHT/interfaces/Joomla/LoginPageUI.java
+++ Joomla_HIENNHT/interfaces/Joomla/LoginPageUI.java
@@ -2,4 +2,7 @@ package Joomla;
 
 public class LoginPageUI {
 
+	public static String LOGIN_USERNAME_TEXTBOX = "//input[@name='username']";
+	public static String LOGIN_PASSWORD_TEXTBOX = "//input[@name='passwd']";
+	public static String LOGIN_BUTTON = "//button[@class='btn btn-primary btn-block btn-large login-button']";
 }
