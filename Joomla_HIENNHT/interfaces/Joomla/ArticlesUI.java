aa88c5e61f4ecf93da1bf825039f23c066f765d5
sherry <sherry@CPU004Sherry>
2017-11-27 16:58:29 +0700

    HienNguyen added 2 interfaces.

---
+++ Joomla_HIENNHT/interfaces/Joomla/ArticlesUI.java
@@ -0,0 +1,9 @@
+package Joomla;
+
+public class ArticlesUI {
+
+	public static String ARTICLE_BUTTON_NEW = "//button[@class='btn btn-small button-new btn-success']";
+	public static String ARTICLE_BUTTON_EDIT = "//button[@class='btn btn-small button-edit']";
+	public static String ARTICLE_BUTTON_PUBLISH = "";
+	public static String ARTICLE_MESSAGE_SUCCESS = "";
+}
---
+++ Joomla_HIENNHT/interfaces/Joomla/ContentUI.java
@@ -0,0 +1,16 @@
+package Joomla;
+
+public class ContentUI {
+	
+	public static String ARTICLE_PAGE_TITLE = "//h1[@class='page-title']";
+	public static String ARTICLE_BUTTON_SAVE = "//button[@class='btn btn-small button-apply btn-success']";
+	public static String ARTICLE_BUTTON_SAVE_CLOSE = "//button[@class='btn btn-small button-save']";
+	public static String ARTICLE_BUTTON_SAVE_NEW = "//button[@class='btn btn-small button-save-new']";
+	public static String ARTILCE_BUTTON_CANCEL = "//button[@class='btn btn-small button-cancel']";
+	
+	public static String CONTENT_TITLE = "//input[@id='jform[title]']";
+	public static String CONTENT_TAB = "//li[@class='active']/a";
+	public static String CONTENT_TEXT = "//textarea[@name='jform[articletext]']";
+	public static String CONTENT_CATEGORY = "//a[@class='chzn-single']//span";
+	
+}
